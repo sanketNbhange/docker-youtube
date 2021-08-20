19ef719357a0826c16c85ba5714269cf6d825aee
sanketNbhange <bhangesanket14@gmail.com>
2021-08-20 15:15:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <p>An amzing startup idea !!!</p>
+       <p>Assignment Done Sanket bhange</p>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
